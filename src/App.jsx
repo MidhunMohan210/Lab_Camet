4d612ba1890837cb9e5e9b918c53454d0f9bcdf7
sreeraj-vijay <sreerajvijay17@gmail.com>
2024-04-29 12:08:32 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import { Suspense, lazy, useEffect, useState } from 'react';
+import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+
+import ECommerce from './pages/Dashboard/ECommerce';
+import SignIn from './pages/Authentication/SignIn';
+import SignUp from './pages/Authentication/SignUp';
+import Loader from './common/Loader/Loader';
+import routes from './routes';
+
+const DefaultLayout = lazy(() => import('./layout/DefaultLayout'));
+
+function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 1000);
+  }, []);
+
+  return loading ? (
+    <Loader />
+  ) : (
+    <Router>
+      <Toaster
+        position="top-right"
+        reverseOrder={false}
+        containerClassName="overflow-auto"
+      />
+      <Routes>
+        <Route path="/auth/signin" element={<SignIn />} />
+        <Route path="/auth/signup" element={<SignUp />} />
+        <Route element={<DefaultLayout />}>
+          <Route index element={<ECommerce />} />
+          {routes.map((route, index) => {
+            const { path, component: Component } = route;
+            return (
+              <Route
+                key={index}
+                path={path}
+                element={
+                  <Suspense fallback={<Loader />}>
+                    <Component />
+                  </Suspense>
+                }
+              />
+            );
+          })}
+        </Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
